56120d764df0e47478ec4740db31e704722ae85c
Peter Dimitrov <113916372+peterdidimitrov@users.noreply.github.com>
2023-08-29 08:35:31 +0300

    Upload tic-tac-toe game

---
+++ src/Game.jsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+import Board from "./Board";
+
+export default function Game() {
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const [currentMove, setCurrentMove] = useState(0);
+  const xIsNext = currentMove % 2 === 0;
+  const currentSquares = history[currentMove];
+
+  function handlePlay(nextSquares) {
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
+  }
+
+  function jumpTo(nextMove) {
+    setCurrentMove(nextMove);
+  }
+  const moves = history.map((squares, move) => {
+    let descrition;
+    if (move > 0) {
+      descrition = `Go to move # ${move}`;
+    } else {
+      descrition = "Go to game start";
+    }
+    return (
+      <li key={move}>
+        <button className="description" onClick={() => jumpTo(move)}>
+          {descrition}
+        </button>
+      </li>
+    );
+  });
+
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
+      </div>
+      <div className="game-info">
+        <ol>{moves}</ol>
+      </div>
+    </div>
+  );
+}
